4a1bb477fa554f6decb28e132aade2a5219faba5
sehwjang <sehwan1023@gmail.com>
2024-09-09 20:11:48 +0900

    Chapter1 dynamic duck

--- Chapter1/Duck.java
+++ Chapter1/Duck.java
@@ -2,15 +2,28 @@ public abstract class Duck {
 	FlyBehavior flyBehavior;
 	QuackBehavior quackBehavior;
 
-	public Duck(){}
+	public Duck() {
+	}
+
+	public void setFlyBehavior(FlyBehavior fb){
+		flyBehavior = fb;
+	}
+
+	public void setQuackBehavior(QuackBehavior qb){
+		quackBehavior = qb;
+	}
+
 	public abstract void display();
-	public void performFly(){
+
+	public void performFly() {
 		flyBehavior.fly();
 	}
-	public void performQuack(){
+
+	public void performQuack() {
 		quackBehavior.quack();
 	}
-	public void swim(){
+
+	public void swim() {
 		System.out.println("모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠.");
 	}
 }
